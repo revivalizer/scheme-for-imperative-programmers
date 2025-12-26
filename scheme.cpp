2b6d1c9bd58b0f453170a6dd8acf9dd6479011a3
Ralph Brorsen <ralphbrorsen@hey.com>
2025-12-26 12:42:34 +0100

    Step 2: Recursive Descent Parsing

--- scheme.cpp
+++ scheme.cpp
@@ -37,3 +37,84 @@ struct string_builder {
         return Start;
     }
 };
+
+struct parser {
+	const char* Current;
+    int Col, Row;
+
+    void Init(const char* Start) {
+		Current = Start;
+		Col = 0;
+		Row = 0;
+	}
+
+    char C() {
+        return Current[0];
+    }
+
+    void Next() {
+        Current++;
+        Col++;
+    }
+
+    bool Match(char Char) {
+        if (C() == Char) {
+            Next();
+            return true;
+        }
+        return false;
+    }
+
+    static bool IsWhitespace(char C) {
+        return C == ' ' || C == '\t' || C == '\n' || C == '\r';
+    }
+
+    void EatWhitespace() {
+        while (IsWhitespace(C()) || C() == ';') {
+            if (C() == '\n') {
+                Row++;
+                Col = 0;
+                Next();
+            } else {
+                Next();
+            }
+        }
+    }
+
+    // NOTE: Change to bool return false if no match
+    void ParseSequenceUntil(string_builder* StringBuilder, char Delimiter) {
+        int IndexCount = 0;
+
+        while (true) {
+            EatWhitespace();
+            if (Match(Delimiter)) {
+                return;
+            } else {
+                if (IndexCount > 0) {
+                    StringBuilder->Char(' ');
+                }
+                Parse(StringBuilder);
+                IndexCount++;
+            }
+        }
+    }
+
+    void Parse(string_builder* StringBuilder) {
+        if (Match('(')) {
+            StringBuilder->Char('(');
+            ParseSequenceUntil(StringBuilder, ')');
+            StringBuilder->Char(')');
+        } else if (Match('#')) {
+            if (Match('t')) {
+                StringBuilder->String("#t");
+            } else if (Match('f')) {
+                StringBuilder->String("#f");
+            }
+        } else {
+            while (IsWhitespace(C())==false && C()!=0 && C()!='(' && C()!=')') {
+                StringBuilder->Char(C());
+                Next();
+            }
+        }
+    }
+};
--- test.cpp
+++ test.cpp
@@ -13,6 +13,30 @@ void ExpectWithMessage(bool Condition, const char* Message)
     std::printf("%s\n", Message);
 }
 
+void ExpectParseResult(const char* Input, const char* Expected)
+{
+    char Buffer[1024];
+    string_builder StringBuilder;
+    StringBuilder.Init(Buffer);
+
+    parser Parser;
+    Parser.Init(Input);
+    Parser.ParseSequenceUntil(&StringBuilder, '\0');
+
+    const char* Actual = StringBuilder.Get();
+    bool Equal = StringEqual(Actual, Expected);
+
+    if (Equal) {
+        std::printf("\"%s\" p-> \"%s\"\n", Input, Expected);
+    } else {
+        std::printf("\033[31m");
+        std::printf("Test failed: \"%s\" p-> \"%s\"\n", Input, Expected);
+        std::printf("Got: \"%s\"\n", Actual);
+        std::printf("\033[0m");
+        std::exit(EXIT_FAILURE);
+    }
+}
+
 #define Expect(CONDITION) ExpectWithMessage(CONDITION, #CONDITION)
 
 void StringHelperTests() {
@@ -28,12 +52,35 @@ void StringHelperTests() {
     Expect(StringEqual(Builder.Char('(').String("Hello ").String("world!").Char(')').Get(), "(Hello world!)"));
 }
 
+void BasicParseTests() {
+    ExpectParseResult("  #t", "#t");
+    ExpectParseResult("#f  ", "#f");
+
+    ExpectParseResult("()", "()");
+    ExpectParseResult("() ()", "() ()");
+    ExpectParseResult(" (   ) ", "()");
+
+    ExpectParseResult(" ( #t ) ", "(#t)");
+    ExpectParseResult(" ( #t #f ) ", "(#t #f)");
+    ExpectParseResult(" ( #t #f )  ( #t #f ) ", "(#t #f) (#t #f)");
+    ExpectParseResult(" ( #t ( #f ) ) ", "(#t (#f))");
+    ExpectParseResult(" ( #t ( #f (#t) ) ) ", "(#t (#f (#t)))");
+
+    ExpectParseResult("a", "a");
+    ExpectParseResult(".eww!@#$%^&*123'", ".eww!@#$%^&*123'");
+
+    ExpectParseResult("(quote (a b c))", "(quote (a b c))");
+    ExpectParseResult("(cdr (quote (a b c)))", "(cdr (quote (a b c)))");
+    ExpectParseResult("(car (cdr (quote (a b c))))", "(car (cdr (quote (a b c))))");
+}
+
 int main(int argc, char** argv)
 {
     (void)argc;
     (void)argv;
 
     StringHelperTests();
+    BasicParseTests();
 
     std::printf("\033[32mAll tests passed.\033[0m\n");
 
